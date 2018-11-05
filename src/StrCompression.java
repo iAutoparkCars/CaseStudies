7781af78ddaa74849653955e9e7c2e5f19d07fc4
Steven <bonillasc@gmail.com>
2018-11-05 16:27:34 -0500

    added some test cases, now to make README

--- src/StrCompression.java
+++ src/StrCompression.java
@@ -8,17 +8,21 @@ public class StrCompression {
 		// test cases
 		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
 		System.out.println(s.compressStr("abc").equals("abc"));
-		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
-		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
-		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
-		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
-		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
-		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
-		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
-		System.out.println(s.compressStr("aabcccccaaa").equals("a2b1c5a3"));
+		System.out.println(s.compressStr("aabbccdd").equals("a2b2c2d2"));
+		System.out.println(s.compressStr("aaaaa").equals("a5"));
+		System.out.println(s.compressStr("").equals(""));
+		System.out.println(s.compressStr("a").equals("a"));
+		System.out.println(s.compressStr("ddddda").equals("d5a1"));
+		System.out.println(s.compressStr("xaaaaaaax").equals("x1a7x1"));
+		System.out.println(s.compressStr("aaaxaaa").equals("a3x1a3"));
 	}
 
 	public String compressStr(String str){
+		
+		// degenrate cases
+		if (str == null) return null;
+		if (str.length() == 0) return "";
+		
 		String compressed = "";
 		StringBuilder sb = new StringBuilder();
 		
